04dd7fef3ad19f2dae6fddbd1a4c36f124b4d285
GuoWei <364199574@qq.com>
2025-10-10 05:50:38 +0800

    将网页独立放在webclient中

--- src/server/WebServer.h
+++ src/server/WebServer.h
@@ -12,31 +12,30 @@ namespace snapshot {
 
 class WebServer {
 public:
-    WebServer(std::shared_ptr<SnapshotManager> snapshot_manager,
-              std::shared_ptr<SnapshotComparator> comparator);
-    ~WebServer();
-    
-    bool start(int port = 8080);
-    void stop();
-    bool is_running() const { return running_; }
-    
-    void set_current_snapshot(std::shared_ptr<SystemSnapshot> snapshot) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        current_snapshot_ = snapshot;
-    }
+  WebServer(std::shared_ptr<SnapshotManager> snapshot_manager,
+            std::shared_ptr<SnapshotComparator> comparator);
+  ~WebServer();
+
+  bool start(int port = 8080);
+  void stop();
+  bool is_running() const { return running_; }
+
+  void set_current_snapshot(std::shared_ptr<SystemSnapshot> snapshot) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    current_snapshot_ = snapshot;
+  }
 
 private:
-    std::shared_ptr<SnapshotManager> snapshot_manager_;
-    std::shared_ptr<SnapshotComparator> comparator_;
-    std::unique_ptr<httplib::Server> server_;
-    std::thread server_thread_;
-    std::atomic<bool> running_{false};
-    
-    std::shared_ptr<SystemSnapshot> current_snapshot_;
-    mutable std::mutex mutex_;
-    
-    void setup_routes();
-    std::string generate_html_dashboard();
+  std::shared_ptr<SnapshotManager> snapshot_manager_;
+  std::shared_ptr<SnapshotComparator> comparator_;
+  std::unique_ptr<httplib::Server> server_;
+  std::thread server_thread_;
+  std::atomic<bool> running_{false};
+
+  std::shared_ptr<SystemSnapshot> current_snapshot_;
+  mutable std::mutex mutex_;
+
+  void setup_routes();
 };
 
 } // namespace snapshot
